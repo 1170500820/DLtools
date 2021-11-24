83b2f92858130cd7950ad120bd84118273abba42
Giere-MacbookAir <1464056258@qq.com>
2021-11-24 10:53:08 +0800

    添加基于词向量的配置项embedding_dim，以及LSTM的配置参数
    Signed-off-by: Giere-MacbookAir <1464056258@qq.com>

--- work/NER/NER_settings.py
+++ work/NER/NER_settings.py
@@ -19,6 +19,11 @@ weibo_ner_tags = ['O'] + list(x[0] + x[1] for x in product(['B', 'I'], [
 ]))
 weibo_nert_tags_idx = {s: i for i, s in enumerate(weibo_ner_tags)}
 
+
+# model
+embedding_dim = 300
+num_layers = 2
+
 # train
 plm_lr = 2e-5
 others_lr = 1e-4
@@ -26,3 +31,5 @@ others_lr = 1e-4
 default_bsz = 8
 default_shuffle = True
 default_train_val_split = 0.9
+
+hidden_size = 256
