dd5f4304e731937749fe412d50812f38f44045f5
Giere-MacbookAir <1464056258@qq.com>
2021-11-27 14:51:51 +0800

    模式识别课程的MNIST实验
    Signed-off-by: Giere-MacbookAir <1464056258@qq.com>

---
+++ work/PR_Lab/mnist.py
@@ -0,0 +1,157 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torch.optim import AdamW
+import numpy as np
+from itertools import chain
+
+from work.PR_Lab import mnist_setting
+
+from type_def import *
+from evaluate.evaluator import BaseEvaluator, KappaEvaluator, PrecisionEvaluator
+from utils import tools
+from utils.data import SimpleDataset
+
+
+class Naive_Linear(nn.Module):
+    def __init__(self, input_dim: int = mnist_setting.input_dim_1, label_cnt: int = mnist_setting.label_cnt_1):
+        super(Naive_Linear, self).__init__()
+        self.input_dim = input_dim
+        self.label_cnt = label_cnt
+        self.linear = nn.Linear(input_dim, input_dim // 2)
+        self.linear2 = nn.Linear(input_dim // 2, label_cnt)
+        self.init_weights()
+
+    def init_weights(self):
+        torch.nn.init.xavier_uniform_(self.linear.weight)
+        self.linear.bias.data.fill_(0)
+        torch.nn.init.xavier_uniform_(self.linear2.weight)
+        self.linear2.bias.data.fill_(0)
+
+    def get_optimizers(self):
+        params = self.linear.parameters()
+        params2 = self.linear2.parameters()
+        linear_optimizer = AdamW(params=chain(params, params2), lr=mnist_setting.lr)
+        return [linear_optimizer]
+    
+    def forward(self, inp: torch.Tensor):
+        """
+        
+        :param inp: (bsz, input_dim)
+        :return: 
+        """
+        out = self.linear(inp)  # (bsz, input_dim // 2)
+        out = F.relu(out)
+        out = self.linear2(out)  # (input_dim //2, label_cnt)
+        out = F.dropout(out, p=0.3)
+        out = F.softmax(out, dim=-1)  # (bsz, label_cnt)
+        return {
+            "mnist_result": out  # (bsz, label_cnt)
+        }
+
+
+class MNIST_Loss(nn.Module):
+    def forward(self, mnist_result: torch.Tensor, mnist_label: torch.Tensor):
+        """
+        
+        :param mnist_result: (bsz, label_cnt)
+        :param mnist_label: (bsz)
+        :return: 
+        """
+        loss = F.cross_entropy(mnist_result, mnist_label)
+        return loss
+
+
+class MNIST_Evaluator(BaseEvaluator):
+    def __init__(self):
+        super(MNIST_Evaluator, self).__init__()
+        self.results = []
+        self.gts = []
+        self.kappa_evaluator = KappaEvaluator()
+        self.precision_evaluator = PrecisionEvaluator()
+
+    def eval_single(self, mnist_result: torch.Tensor, mnist_gt: torch.Tensor):
+        """
+
+        :param mnist_result: (1, label_cnt)
+        :param mnist_gt: (1)
+        :return:
+        """
+        mnist_result = mnist_result.squeeze()  # (label_cnt)
+        _, max_idx = torch.max(mnist_result, 0)  # (1)
+        max_idx = float(max_idx.clone().detach())
+        self.kappa_evaluator.eval_single(max_idx, mnist_gt)
+        self.precision_evaluator.eval_single(max_idx, mnist_gt)
+
+    def eval_step(self) -> Dict[str, Any]:
+        kappa = self.kappa_evaluator.eval_step()
+        prec = self.precision_evaluator.eval_step()
+        kappa.update(prec)
+        return kappa
+
+
+def train_dataset_factory(samples: List[List[float]], labels: List[int], bsz = mnist_setting.bsz, shuffle=True):
+    data_dicts = list({'sample': np.array(x), 'label': y} for (x, y) in zip(samples, labels))
+    train_dataset = SimpleDataset(data_dicts)
+
+    def collate_fn(lst):
+        dict_of_data = tools.transpose_list_of_dict(lst)
+        sample = torch.tensor(np.stack(dict_of_data['sample']), dtype=torch.float)
+        label = torch.tensor(np.stack(list(np.array(x) for x in dict_of_data['label'])), dtype=torch.long)
+        return {
+            "inp": sample,  # (bsz, input_dim),
+        }, {
+            "mnist_label": label  # (bsz)
+        }
+    train_dataloader = DataLoader(train_dataset, batch_size=bsz, shuffle=shuffle, collate_fn=collate_fn)
+    return train_dataloader
+
+
+def val_dataset_factory(samples: List[List[float]], labels: List[int]):
+    data_dicts = list({'sample': np.array(x), 'label': y} for (x, y) in zip(samples, labels))
+    val_dataset = SimpleDataset(data_dicts)
+
+    def collate_fn(lst):
+        dict_of_data = tools.transpose_list_of_dict(lst)
+        sample = torch.tensor(np.stack(dict_of_data['sample']), dtype=torch.float)
+
+        return {
+            'inp': sample,  # (1, input_dim)
+        }, {
+            'mnist_gt': dict_of_data['label'][0]
+        }
+
+    val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=False, collate_fn=collate_fn)
+    return val_dataloader
+
+
+def dataset_factory(file_dir: str, bsz: int = mnist_setting.bsz, shuffle=True):
+    label_path, sample_path = 'TrainLabels.csv', 'TrainSamples.csv'
+    if file_dir[-1] != '/':
+        file_dir += '/'
+    label_path = file_dir + label_path
+    sample_path = file_dir + sample_path
+
+    labels = list(int(x) for x in open(label_path, 'r').read().strip().split('\n'))  # List[int]
+    samples = open(sample_path, 'r').read().strip().split('\n')
+    samples = list(list(float(x) for x in a.split(',')) for a in samples)  # List[List[float]]
+
+    train_samples, val_samples = tools.split_list_with_ratio(samples, mnist_setting.train_val_split)
+    train_labels, val_labels = tools.split_list_with_ratio(labels, mnist_setting.train_val_split)
+
+    train_dataloader = train_dataset_factory(train_samples, train_labels, bsz=bsz, shuffle=shuffle)
+    val_dataloader = val_dataset_factory(val_samples, val_labels)
+
+    return train_dataloader, val_dataloader
+
+
+model_registry = {
+    "model": Naive_Linear,
+    "loss": MNIST_Loss,
+    "evaluator": MNIST_Evaluator,
+    "dataset": dataset_factory,
+    'args': [
+        {'name': "--file_dir", 'dest': 'file_dir', 'type': str, 'help': '训练/测试数据文件的路径'}
+    ]
+}
