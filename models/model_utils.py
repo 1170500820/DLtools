27daa67f6d5d83e9080b3a9b7beb30e84d59ac8c
Giere-MacbookAir <1464056258@qq.com>
2021-12-14 10:47:04 +0800

    加入UniversalUseModel，输入模型、保存路径等，输出一个callable，把模型包装成方便可调用等格式
    Signed-off-by: Giere-MacbookAir <1464056258@qq.com>

--- models/model_utils.py
+++ models/model_utils.py
@@ -1,3 +1,6 @@
+import pickle
+import torch
+
 from type_def import *
 
 
@@ -7,3 +10,34 @@ def get_init_params(local_dict: Dict[str, Any]):
         if key[0] != '_' and key != 'self':
             result_dict[key] = value
     return result_dict
+
+
+class UniversalUseModel:
+    def __init__(self,
+                 state_dict_path: str,
+                 init_params_path: str,
+                 model_class,
+                 sample_to_train_input: Callable,
+                 train_output_to_read_format: Callable):
+        """
+        默认是把模型放在cpu上了
+        :param state_dict_path:
+        :param init_params_path:
+        :param model_class:
+        :param sample_to_train_input:
+        :param train_output_to_read_format:
+        """
+        self.state_dict_path = state_dict_path
+        self.init_params_path = init_params_path
+        self.sample_to_train_input = sample_to_train_input
+        self.train_output_to_read_format = train_output_to_read_format
+
+        init_params = pickle.load(open(self.init_params_path, 'rb'))
+        self.model = model_class(**init_params)
+        self.model.load_state_dict(torch.load(open(self.state_dict_path, 'rb'), map_location=torch.device('cpu')))
+
+    def __call__(self, input_sample):
+        train_input = self.sample_to_train_input(input_sample)
+        output = self.model(**train_input)
+        result = self.train_output_to_read_format(output)
+        return result
