8964c305e1bc95b9c5f2a9d21de49d623695f835
Giere-MacbookAir <1464056258@qq.com>
2022-01-15 10:36:59 +0800

    修正了dir_deepsearch的实现，并开始实现dir_deepsearch_with_pattern，但是正则匹配的模式不知道怎么做，就先放着
    Signed-off-by: Giere-MacbookAir <1464056258@qq.com>

--- utils/dir_tools.py
+++ utils/dir_tools.py
@@ -2,19 +2,45 @@
 目录下多文件处理相关的函数
 """
 from type_def import *
-from pathlib import Path
+import os
+import re
 
 
-def dir_deepsearch(root_dir, r_pattern):
+def dir_deepsearch(root_dir):
     """
-    对于root_dir进行深度搜索，找出所有符合特征的文件名
-    :param root_dir: 开始deepsearch的根目录
-    :param r_pattern: 需要寻找的文件名的特征
+    来自https://thispointer.com/python-how-to-get-list-of-files-in-directory-and-sub-directories/
+    对一个路径进行遍历搜索，获取其中的所有文件，而不是文件夹
+    :param root_dir:
     :return:
     """
-    root_path = Path(root_dir)
-    filenames = list(root_path.rglob(r_pattern))
-    return filenames
+    # create a list of file and sub directories
+    # names in the given directory
+    listOfFile = os.listdir(root_dir)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(root_dir, entry)
+        # If entry is a directory then get the list of files in this directory
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + dir_deepsearch(fullPath)
+        else:
+            allFiles.append(fullPath)
+
+    return allFiles
 
 
-
+def dir_deepsearch_with_pattern(root_dir, pattern):
+    """
+    对一个路径进行深度遍历搜索，获取所有文件名，不包括文件夹
+    然后对文件名进行判断，筛选出符合pattern的
+    todo 这个pattern该如何定义呢？使用re模块的话，就需要考虑是否适配r字符串。不过路径似乎不需要太复杂的匹配？绝大多数情况都是看后缀和前缀
+    :param root_dir:
+    :param pattern:
+    :return:
+    """
+    correct_path = []
+    path_found = dir_deepsearch(root_dir)
+    for elem_path in path_found:
+        path_components = os.path.split(elem_path)
+    raise NotImplementedError
