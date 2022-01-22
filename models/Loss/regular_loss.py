54b8b4689f730e11a76025bbccc4aef214788e07
zshuzhezhou <zshuzhezhou@tencent.com>
2022-01-23 03:03:59 +0800

    - 删除CRF_Loss，因为allennlp的版本会带来各种不兼容 - 修改了Scalar_Loss的计算方式，现在使用mse_loss
    Signed-off-by: zshuzhezhou <zshuzhezhou@tencent.com>

--- models/Loss/regular_loss.py
+++ models/Loss/regular_loss.py
@@ -4,23 +4,32 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from allennlp.modules import ConditionalRandomField
-
-
-class CRF_Loss(nn.Module):
-    def __init__(self):
-        super(CRF_Loss, self).__init__()
-
-    def forward(self, crf: ConditionalRandomField, token_scores, tags, token_mask):
-        batch_size = token_scores.shape[0]
-        loss = -crf(token_scores, tags, token_mask) / float(batch_size)
-        return loss
+# from allennlp.modules import ConditionalRandomField
+#
+#
+# class CRF_Loss(nn.Module):
+#     def __init__(self):
+#         super(CRF_Loss, self).__init__()
+#
+#     def forward(self, crf: ConditionalRandomField, token_scores, tags, token_mask):
+#         batch_size = token_scores.shape[0]
+#         loss = -crf(token_scores, tags, token_mask) / float(batch_size)
+#         return loss
 
 
 class Scalar_Loss(nn.Module):
+    """
+    标量loss针对最简单的loss
+    """
     def __init__(self):
         super(Scalar_Loss, self).__init__()
 
     def forward(self, output: torch.Tensor, label: torch.Tensor):
-        return output - label
+        """
+
+        :param output:
+        :param label:
+        :return:
+        """
+        return F.mse_loss(output, label)
 
